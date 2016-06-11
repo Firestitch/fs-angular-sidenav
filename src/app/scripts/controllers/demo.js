3dd7a5b7b8fc5a470c83046635f55894840e0b85
Raymond <ray@firestitch.com>
2016-06-11 12:53:23 -0400

    commit

---
+++ src/app/scripts/controllers/demo.js
@@ -0,0 +1,19 @@
+'use strict';
+
+
+angular.module('app')
+  .controller('DemoCtrl', function ($scope) {
+
+  	$scope.date1 = new Date();
+  	$scope.date1.setDate($scope.date1.getDate()-5);
+
+  	$scope.date2 = new Date();
+  	$scope.date2.setMinutes($scope.date2.getMinutes()-30);
+
+  	$scope.date3 = new Date();
+  	$scope.date3.setMinutes($scope.date3.getMinutes()-1);
+
+  	$scope.date4 = new Date();
+    
+});
+
---
+++ src/app/scripts/directives/directive.js
@@ -0,0 +1,41 @@
+(function () {
+    'use strict';
+
+    angular.module('fs-angular-sidenav',[])
+    .directive('fsSidenav', function() {
+        return {
+            restrict: 'E',
+            scope: {
+                selected: '=fsSelected',
+                width: '=fsWidth'
+            },
+            link: function($scope, element, attrs) {
+
+                if($scope.width) {
+                    angular.element(element).css('width',$scope.width + 'px');
+                }
+
+                var items = element[0].querySelectorAll('fs-sidenav-side fs-sidenav-item');
+
+                angular.forEach(items,function(item) {
+                    angular.element(item).on('click',function() {
+
+                        angular.element(element[0].querySelectorAll('fs-sidenav-content fs-sidenav-item')).css('display','none');
+
+                        var item = element[0].querySelector('fs-sidenav-content fs-sidenav-item[fs-id="' + angular.element(this).attr('fs-id') + '"]');
+
+                        if(item) {
+                            angular.element(item).css('display','block');
+                        }                        
+                    });
+                });
+
+                if($scope.selected) {
+                    var item = element[0].querySelector('fs-sidenav-side fs-sidenav-item[fs-id="' + $scope.selected + '"]');
+                    angular.element(item).triggerHandler('click');
+                }
+            }
+        };
+    });  
+
+})();
