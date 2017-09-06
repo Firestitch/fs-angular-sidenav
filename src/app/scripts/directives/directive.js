8d565a9b8885da503046e12e59685ac76905e358
Raymond <ray@firestitch.com>
2017-09-06 13:37:16 -0400

    subnav text highlight

--- src/app/scripts/directives/directive.js
+++ src/app/scripts/directives/directive.js
@@ -170,7 +170,7 @@
 	.directive('fsSidenavSubitem', function($location, fsUtil, $rootScope, fsSidenav, fsTheme) {
     	return {
 	        restrict: 'E',
-	        template: '<div class="fs-sidenav-subitem" ng-style="selected && { backgroundColor: color }" ng-class="{ selected: selected }"><a ng-href="{{href}}" ng-click="clicked(e)" ng-transclude></a></div>',
+	        template: '<div class="fs-sidenav-subitem" ng-class="{ selected: selected }" ng-style="selected && { color: color }"><a ng-href="{{href}}" ng-click="clicked(e)" ng-transclude></a></div>',
 	        transclude: true,
 	        replace: true,
 	        scope: {
@@ -182,7 +182,8 @@
 	        },
 	        require: ['^fsSidenav','^fsSidenavItem'],
 	        controller: function($scope) {
-	        	$scope.color = fsTheme.primaryRGBa(.2);
+
+	        	$scope.color = fsTheme.primaryHex();
 
 	        	if(!$scope.name) {
 	        		$scope.name = fsUtil.guid();
